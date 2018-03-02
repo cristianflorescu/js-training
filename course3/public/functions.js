1c435eba3f3629929742208e5ad3f042c5586982
Andy Ghiuta <andy.ghiuta@gmail.com>
2018-03-02 08:40:07 +0200

    Course 3 solutions

--- course3/public/functions.js
+++ course3/public/functions.js
@@ -11,9 +11,12 @@ let setOutputValue = function(button, out, selector = '.output') {
 // algorithm functions
 let leapYear = function() {
   // read the input value
-  let input = getInputValue(this);
-  // TODO: calculate the output somehow
-  let output = '';
+  let input = parseInt(getInputValue(this), 10);
+  // calculate the output
+  let output = 'No';
+  if (0 === input % 400 || (0 === input % 4 && 0 !== input % 100)) {
+    output = 'Yes'
+  }
   // set the output value
   setOutputValue(this, output);
 };
@@ -21,16 +24,63 @@ let leapYear = function() {
 let fibonacci = function() {
   // read the input value
   let input = getInputValue(this);
-  // TODO: calculate the output somehow
-  let output = '';
+  // calculate the output
+  let fibonacciRecursive = function(n, memo = {}) {
+    // "caching" - if we have computed this value already, return it
+    if (memo[n]) {
+      return memo[n];
+    }
+    // stop condition
+    if (n === 0 || n === 1) {
+      return n;
+    }
+    // calculate
+    memo[n] = fibonacciRecursive(n - 1, memo) + fibonacciRecursive(n - 2, memo);
+    return memo[n];
+  };
+  let memo = {},
+    fibNr = fibonacciRecursive(parseInt(input, 10), memo);
+  let output = Object.values(memo).join(', ');
   // set the output value
   setOutputValue(this, output);
 };
+
+String.prototype.reverse = function() {
+  return this.split('').reverse().join('')
+};
+
+let reverse = function() {
+  // read the input value
+  let input = getInputValue(this);
+  // calculate the output
+  let output = input.reverse();
+  // set the output value
+  setOutputValue(this, output);
+};
+
+
+let Palindrome = function() {
+  this.removeSpaces = function(input) {
+    return input.replace(/\s/, '');
+  }
+};
+
+Palindrome.prototype.check = function(input) {
+  // prepare the input value
+  input = this.removeSpaces(input);
+  // calculate the output
+  return input.reverse === input;
+}
 
 // Register click handlers
 
 // NOTE: showAlert(); or showAlert(param); will NOT work here.
 // Must be a reference to a function name, not a function call.
-document.getElementById('leapYearBtn').onclick = fibonacci;
-document.getElementById('fibonacciBtn').onclick = leapYear;
-// TODO: add the other two button click handlers
+document.getElementById('leapYearBtn').onclick = leapYear;
+document.getElementById('fibonacciBtn').onclick = fibonacci;
+document.getElementById('reverseBtn').onclick = reverse;
+let palindrome = new Palindrome();
+document.getElementById('palindromeBtn').onclick = function() {
+  let isPalindrome = palindrome.check(getInputValue(this));
+  setOutputValue(this, isPalindrome ? 'Yes' : 'No');
+};
